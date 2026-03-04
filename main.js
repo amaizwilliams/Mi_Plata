fdc14a3a48da1f93c4dce63fce4fac46cd4e4dfb
Williams Amaiz <dwilliamsa63@gmail.com>
2026-03-04 16:06:11 -0500

    5th commit

--- main.js
+++ main.js
@@ -41,41 +41,45 @@
         let intentosUsuario = 0;
         let errorUsuarioEntrada = true;
 
-        while (errorUsuarioEntrada) {
+        while(errorUsuarioEntrada) {
             let usuarioEntrada = prompt("Usuario:");
-            for (let i = 0; i < usuariosBancarios.length; i++) {
-                if (usuarioEntrada === usuariosBancarios[i].nombreUsuario) {
-                    if (usuariosBancarios[i].estado === false) {
-                        console.log(`Su cuenta se encuentra bloqueada.`);
-                        errorUsuarioEntrada = false;
-                        break;
-                    } else {
-                        let passwordEntrada = prompt("Contraseña:");
-                        if (passwordEntrada === `` || passwordEntrada === ` ` || passwordEntrada === null) {
-                            console.log(`La contraseña no puede estar vacia.`);
+            if (usuarioEntrada === `` || usuarioEntrada === ` ` || usuarioEntrada === null) {
+                console.log(`El campo 'Usuario' no puede estar vacio.`);
+            } else {
+                for (let i = 0; i < usuariosBancarios.length; i++) {
+                    if (usuarioEntrada === usuariosBancarios[i].nombreUsuario) {
+                        if (usuariosBancarios[i].estado === false) {
+                            console.log(`Su cuenta se encuentra bloqueada.`);
+                            errorUsuarioEntrada = false;
+                            break;
                         } else {
-                            if (passwordEntrada === usuariosBancarios[i].password) {
-                                console.log("Sesión con éxito");
-                                transacciones(usuariosBancarios[i]);
-                                return;
+                            let passwordEntrada = prompt("Contraseña:");
+                            if (passwordEntrada === `` || passwordEntrada === ` ` || passwordEntrada === null) {
+                                console.log(`La contraseña no puede estar vacia.`);
                             } else {
-                                intentosUsuario ++;
-                                if (intentosUsuario < 3) {
-                                    console.log(`La contraseña es incorrecta. Lleva ${intentosUsuario} intento/s`);
+                                if (passwordEntrada === usuariosBancarios[i].password) {
+                                    console.log("Sesión con éxito");
+                                    transacciones(usuariosBancarios[i]);
+                                    return;
+                                } else {
+                                    intentosUsuario ++;
+                                    if (intentosUsuario < 3) {
+                                        console.log(`La contraseña es incorrecta. Lleva ${intentosUsuario} intento/s`);
+                                    }
+                                    if (intentosUsuario === 3) {
+                                        usuariosBancarios[i].estado = false;
+                                        actualizarStorage();
+                                        console.log(`Su cuenta a sido bloqueada por 3 intentos fallidos. Debera esperar 24 horas`);
+                                        errorUsuarioEntrada = false;
+                                    }
+                                    break;
                                 }
-                                if (intentosUsuario === 3) {
-                                    usuariosBancarios[i].estado = false;
-                                    actualizarStorage();
-                                    console.log(`Su cuenta a sido bloqueada por 3 intentos fallidos. Debera esperar 24 horas`);
-                                    errorUsuarioEntrada = false;
-                                }
-                                break;
                             }
                         }
-                    }
-                } else {
-                    if (i == (usuariosBancarios.length - 1)) {
-                        console.log(`El usuario '${usuarioEntrada}' no existe en el sistema`);
+                    } else {
+                        if (i == (usuariosBancarios.length - 1)) {
+                            console.log(`El usuario '${usuarioEntrada}' no existe en el sistema`);
+                        }
                     }
                 }
             }
@@ -168,7 +172,7 @@
         menuTransacciones = prompt(`--Transacciones--\n
             1. Retirar\n
             2. Consultar saldo\n
-            3. consignar\n
+            3. Consignar\n
             4. Consultar Movimientos\n
             5. Transferir\n
             6. Cerrar sesion
@@ -206,22 +210,27 @@
         };
     }
 //----------------------------------------------------------------------------------------------------------
-    function retirar(nombreUsuario){
+    function retirar(nombreUsuario) {
         let montoRetirarEntrada
         let errorMontoRetirarEntrada = true
 
         while(errorMontoRetirarEntrada) {
-            montoRetirarEntrada = parseFloat(prompt("Monto a retirar: "))
-            if(montoRetirarEntrada <= 0 ){
-                console.log(`El monto no puede ser 0 o menor.`);
-            }else{
-                if (montoRetirarEntrada > nombreUsuario.saldo){
-                    console.log("¡El saldo es insuficiente!");
+            montoRetirarEntrada = prompt("Monto a retirar: ")
+            if (montoRetirarEntrada === `` || montoRetirarEntrada === ` ` || montoRetirarEntrada === null) {
+                console.log(`El campo no puede estar vacio`);
+            } else {
+                montoRetirarEntrada = parseFloat(montoRetirarEntrada);
+                if(montoRetirarEntrada <= 0) {
+                    console.log(`El monto no puede ser 0 o menor.`);
                 } else {
-                    nombreUsuario.saldo-=montoRetirarEntrada
-                    nombreUsuario.movimientos.push(`${new Date().toLocaleString()} Retiro: -$${montoRetirarEntrada}`);
-                    actualizarStorage()
-                    errorMontoRetirarEntrada = false;
+                    if (montoRetirarEntrada > nombreUsuario.saldo){
+                        console.log("¡El saldo es insuficiente!");
+                    } else {
+                        nombreUsuario.saldo -= montoRetirarEntrada
+                        nombreUsuario.movimientos.push(`${new Date().toLocaleString()} Retiro: -$${montoRetirarEntrada}`);
+                        actualizarStorage()
+                        errorMontoRetirarEntrada = false;
+                    }
                 }
             }
         }
@@ -237,24 +246,30 @@
 //----------------------------------------------------------------------------------------------------------
     function consignarSaldo(nombreUsuario){
         let montoConsignarSaldo
-        let errorConsignarSaldo=false
-        do{
-            montoConsignarSaldo=parseFloat(prompt("Monto a consignar: "))
+        let errorConsignarSaldo = true
+
+        while(errorConsignarSaldo) {
+            montoConsignarSaldo = prompt("Monto a consignar: ")
+            if (montoConsignarSaldo === `` || montoConsignarSaldo === ` ` || montoConsignarSaldo === null) {
+                console.log(`El campo no puede estar vacio`);
+            } else {
+                montoConsignarSaldo = parseFloat(montoConsignarSaldo);
                 if(montoConsignarSaldo <= 0){
                     console.log("¡El campo es incorrecto!");
                 }else{
                     nombreUsuario.saldo += montoConsignarSaldo
                     nombreUsuario.movimientos.push(`${new Date().toLocaleString()} Consignacion: +$${montoConsignarSaldo}`);
                     actualizarStorage()
-                    errorConsignarSaldo=true
+                    errorConsignarSaldo = false
                 }
-        }while(errorConsignarSaldo==false);
+            }
+        };
         console.log("!Consignacion con exito¡");
         console.log("Su saldo actual es de: "+nombreUsuario.saldo);
     }
 //----------------------------------------------------------------------------------------------------------
     function consultarMovimientos(nombreUsuario){
-    console.log(`Movimientos`);
+    console.log(`Movimientos:`);
     if(nombreUsuario.movimientos.length===0){
         console.log("No hay moviemientos");
     }
@@ -266,52 +281,45 @@
     function transferir(nombreUsuario){
         let nombreUsuarioTransferirEntrada
         let montoTransferirEntrada
-        let usuarioEncontradoTranferir=false
-        let errorTransferir=false
-        do{
-            nombreUsuarioTransferirEntrada=prompt("usuario: ")
-            montoTransferirEntrada=parseFloat(prompt("Monto: "))
-        for(let i=0; i<usuariosBancarios.length; i++){
-            if(nombreUsuarioTransferirEntrada===usuariosBancarios[i].nombreUsuario){
-                usuarioEncontradoTranferir=true 
-                if(montoTransferirEntrada>nombreUsuario.saldo){
-                    console.log("Saldo insuficiente");
-                    errorTransferir=true
-                    break;
+        let errorTransferir = true
+
+        while(errorTransferir) {
+            montoTransferirEntrada = prompt("Ingrese el monto a tranferir: ")
+            if (montoTransferirEntrada === `` || montoTransferirEntrada === ` ` || montoTransferirEntrada === null) {
+                console.log(`El campo no puede estar vacio`);
+            } else {
+                montoTransferirEntrada = parseFloat(montoTransferirEntrada)
+                if (montoTransferirEntrada <= 0) {
+                    console.log(`El campo no puede ser 0 o un numero negativo`);
+                } else {
+                    if (montoTransferirEntrada > nombreUsuario.saldo) {
+                        console.log("Saldo insuficiente");
+                        errorTransferir = false
+                    } else {
+                        nombreUsuarioTransferirEntrada = prompt("Usuario: ")
+                        if (nombreUsuarioTransferirEntrada === `` || nombreUsuarioTransferirEntrada === ` ` || nombreUsuarioTransferirEntrada === null) {
+                            console.log(`El campo no puede estar vacio`);
+                        } else {
+                            for(let i=0; i<usuariosBancarios.length; i++) {
+                                if (nombreUsuarioTransferirEntrada === usuariosBancarios[i].nombreUsuario) {
+                                    nombreUsuario.saldo -= montoTransferirEntrada
+                                    usuariosBancarios[i].saldo += montoTransferirEntrada
+                                    nombreUsuario.movimientos.push(`${new Date().toLocaleString()} Transferencia enviada a ${usuariosBancarios[i].nombreUsuario}: -$${montoTransferirEntrada}`);
+                                    usuariosBancarios[i].movimientos.push(`${new Date().toLocaleString()} Transferencia recibida de ${nombreUsuario.nombreUsuario}: +$${montoTransferirEntrada}`);
+                                    actualizarStorage()
+                                    errorTransferir = false;
+                                    console.log("¡Transferencia con exito!");
+                                    console.log("Su saldo actual es: "+nombreUsuario.saldo);
+                                } else {
+                                    if (i == (usuariosBancarios.length - 1)) {
+                                        console.log(`El usuario no existe`);
+                                        errorTransferir = false;
+                                    }
+                                }
+                            }
+                        }
+                    }   
                 }
-                if(montoTransferirEntrada<=0){
-                    console.log(`El campo no puede ser '${montoTransferirEntrada}'`);
-                    errorTransferir=true 
-                    break;
-                }
-                nombreUsuario.saldo-=montoTransferirEntrada
-                usuariosBancarios[i].saldo+=montoTransferirEntrada
-                nombreUsuario.movimientos.push(`${new Date().toLocaleString()} Transferencia enviada a ${usuariosBancarios[i].nombreUsuario}: -$${montoTransferirEntrada}`);
-                usuariosBancarios[i].movimientos.push(`${new Date().toLocaleString()} Transferencia recibida de ${nombreUsuario.nombreUsuario}: +$${montoTransferirEntrada}`);
-                actualizarStorage()
-                errorTransferir=true
-                console.log("¡Transferencia con exito!");
-                console.log("Su saldo actual es: "+nombreUsuario.saldo);
             }
-        }
-            if(usuarioEncontradoTranferir===false){
-                let salirTransferencia
-                let errorSalirTransferencia=false
-                do{
-                    salirTransferencia= parseInt(prompt(`El usuario no existe, desea salir (1.Si / 2.No)`));
-                    switch(salirTransferencia){
-                        case 1:
-                            errorTransferir=true
-                            errorSalirTransferencia=true
-                            break;
-                        case 2: 
-                            errorSalirTransferencia=true
-                            break;
-                        default:
-                            console.log(`La opcion '${salirTransferencia}' no es valida`);
-                            break;
-                    }
-                }while(errorSalirTransferencia==false);
-            } 
-        }while(errorTransferir===false);
+        };
     }
